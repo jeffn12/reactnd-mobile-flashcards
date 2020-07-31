fd535a8477b85e4df9dc6a1d1b9bb26792f3fed0
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-31 11:02:40 -0400

    feat: add components to App for testing

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+// Redux
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import middleware from "./middleware";
+// Components
+import DeckList from "./components/DeckList";
+import AddDeck from "./components/AddDeck";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={createStore(reducer, middleware)}>
+      <View style={styles.container}>
+        <Text>My First React Native App!</Text>
+        <AddDeck />
+        <DeckList />
+      </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#aaa",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
