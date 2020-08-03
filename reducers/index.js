d4fad179be30175e0d2a0a4c0b9e7b702b53073e
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-03 16:15:05 -0400

    refactor: remove unused actions/reducer cases

--- reducers/index.js
+++ reducers/index.js
@@ -1,28 +1,10 @@
-import { RECEIVE_DECKS, ADD_DECK, ADD_CARD_TO_DECK } from "../actions";
+import { RECEIVE_DECKS } from "../actions";
 
 export const decks = (state = { decks: {} }, action) => {
   switch (action.type) {
     case RECEIVE_DECKS:
       return {
         decks: { ...state.decks, ...action.decks }
-      };
-    case ADD_DECK:
-      return {
-        decks: {
-          ...state,
-          [action.title]: { title: action.title, questions: [] }
-        }
-      };
-    case ADD_CARD_TO_DECK:
-      const { title, card } = action;
-      const update = Object.assign({}, state[title], {
-        questions: state[title].questions.concat([card])
-      });
-      return {
-        decks: {
-          ...state,
-          [action.title]: update
-        }
       };
     default:
       return state;
