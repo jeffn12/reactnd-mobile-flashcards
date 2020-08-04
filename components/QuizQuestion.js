dfc54215d183eb3acf63e3a73ed61178c0b9dbf5
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-04 12:18:33 -0400

    feat: add basic stylesheet

--- components/QuizQuestion.js
+++ components/QuizQuestion.js
@@ -1,23 +1,55 @@
 import React, { Component } from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
 
 export class QuizQuestion extends Component {
   render() {
     const { handleAnswer } = this.props;
     const { question, answer } = this.props.question;
     return (
-      <View>
-        <Text> Question: {question} </Text>
-        <Text> Answer: {answer} </Text>
-        <TouchableOpacity onPress={() => handleAnswer("yes")}>
-          <Text>YES</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleAnswer("no")}>
-          <Text>NO</Text>
-        </TouchableOpacity>
+      <View style={Styles.container}>
+        <Text style={Styles.questionLabel}> Question: {question} </Text>
+        <Text style={Styles.answerLabel}> Answer: {answer} </Text>
+        <View style={Styles.btnContainer}>
+          <TouchableOpacity
+            style={Styles.btn}
+            onPress={() => handleAnswer("yes")}
+          >
+            <Text>YES</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={Styles.btn}
+            onPress={() => handleAnswer("no")}
+          >
+            <Text>NO</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
 
 export default QuizQuestion;
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "90%",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginTop: "5px"
+  },
+  btnContainer: {
+    flex: 1,
+    flexDirection: "row"
+  },
+  quizHead: {
+    fontSize: "1rem"
+  },
+  btn: {
+    flex: 1,
+    backgroundColor: "blue",
+    alignItems: "center",
+    margin: "3px",
+    padding: "15px"
+  }
+});
