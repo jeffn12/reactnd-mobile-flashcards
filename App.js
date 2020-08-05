cb471d90430f9da10f094d05ad9b1706bf8379b9
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-05 10:34:13 -0400

    feat: add a stack navigator into tabs

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
 import { enableScreens } from "react-native-screens";
 // Redux
 import { createStore } from "redux";
@@ -13,13 +14,26 @@ import AddDeck from "./components/AddDeck";
 
 enableScreens();
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const DeckStackScreen = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="My Flashcard Decks"
+        component={DeckList}
+        options={{ tabBarLabel: "My Decks" }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <Provider store={createStore(reducer, middleware)}>
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="Decks" component={DeckList} />
+          <Tab.Screen name="My Decks" component={DeckStackScreen} />
           <Tab.Screen name="Add a Deck" component={AddDeck} />
         </Tab.Navigator>
       </NavigationContainer>
